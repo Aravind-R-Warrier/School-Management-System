1a4e392030c5dc42db819c554e6dc672406d1bb9
Aravind-R-Warrier <aravindrwarrier1@gmail.com>
2025-02-11 02:54:00 +0530

    -footer

--- frontend/src/client/utilityComponents/footer/Footer.jsx
+++ frontend/src/client/utilityComponents/footer/Footer.jsx
@@ -1,15 +1,29 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 function Footer() {
   return (
-    <div>
-      <Box sx={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}} component={'div'}>
-        <Typography variant='h5'>School Management System</Typography>
-        <Typography variant='p'>Copyright@2055</Typography>
-      </Box>
-    </div>
-  )
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#2c3e50',
+        // color: '#ecf0f1',
+        padding: '20px',
+        marginTop: '20px',
+      }}
+    >
+      <img
+        src="https://uxwing.com/wp-content/themes/uxwing/download/education-school/graduation-cap-icon.png"
+        alt="School Logo"
+        style={{ height: '50px', padding: '4px', marginBottom: '10px',position:'absolute',left:'639px',bottom:'-586px' }}
+      />
+      <Typography color='black' variant='h5' sx={{ fontWeight: 'bold' }}>School Breeze</Typography>
+      <Typography color='black' variant='body2'>Copyright &copy; 2055 | All Rights Reserved</Typography>
+    </Box>
+  );
 }
 
-export default Footer
+export default Footer;
