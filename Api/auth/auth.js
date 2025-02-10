3229742c4fe641b2da96e1db96a13ededec27a9e
Aravind-R-Warrier <aravindrwarrier1@gmail.com>
2025-02-10 15:02:47 +0530

    --

--- Api/auth/auth.js
+++ Api/auth/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const AuthMiddleware = (roles = []) => {
     return (req, res, next) => {
         try {
-            const token = req.header("Authorization")?.replace("Bearer ", ""); // Fixed spelling
+            const token = req.header("Authorization")?.replace("Bearer ", ""); 
             if (!token) {
                 return res.status(401).json({ success: false, message: "No token provided. Authorization denied." });
             }
--- frontend/src/school/components/dashboard/Dashboard.jsx
+++ frontend/src/school/components/dashboard/Dashboard.jsx
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import { baseApi } from "../../../environment";
 import React, { useEffect, useState, useRef } from 'react';
-import { Box, Button, CardMedia, TextField, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, CardMedia, Paper, TextField, Typography } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import { toast } from 'react-toastify';
+import { PieChart } from '@mui/x-charts/PieChart';
+
+
 function Dashboard() {
   const [school, setSchool] = useState(null);
-  const[schoolName,setSchoolName]=useState('')
+  const [schoolName, setSchoolName] = useState('')
   const [edit, setEdit] = useState(false);
-  const[classes,setClasses]=useState([])
+  const [classes, setClasses] = useState(0);
+  const [students, setStudents] = useState(0);
+  const [studentsDetails, setStudentsDetails] = useState([]);
+  const [examinations, setAllExamination] = useState(0);
 
   // Image handling
   const [file, setFile] = useState(null);
@@ -16,10 +22,10 @@ function Dashboard() {
 
   const addImage = (event) => {
     const file = event.target.files[0];
-  
+
     setImageUrl(URL.createObjectURL(file));
     setFile(file);
-};
+  };
 
 
   const fileinputRef = useRef(null);
@@ -46,58 +52,88 @@ function Dashboard() {
 
 
 
-  const handleEditSubmit=async()=>{
-    
+  const handleEditSubmit = async () => {
 
-    const fd=new FormData()
-    fd.append('school_name',schoolName)
-    if(file){
-      fd.append('image',file,file.name)
+
+    const fd = new FormData()
+    fd.append('school_name', schoolName)
+    if (file) {
+      fd.append('image', file, file.name)
     }
-    axios.patch(`${baseApi}/school/update`,fd).then(res=>{
+    axios.patch(`${baseApi}/school/update`, fd).then(res => {
       // console.log('school edit:',res)
       toast.success('Edit Successfull')
       fetchSchool();
       handleCancelEdit()
-    }).catch((err)=>{
+    }).catch((err) => {
       console.log(err)
-      toast.error('edit failed',err)
+      toast.error('edit failed')
     })
   }
 
-  const handleCancelEdit=()=>{
+  const handleCancelEdit = () => {
     setEdit(false)
     handleClearFile()
   }
 
- 
+
 
   // fetchStudents
-  const fetchNumberOfClasses=async()=>{
-  await  axios.get(`${baseApi}/class/all`).then(res=>{
+  const fetchNumberOfClasses = async () => {
+    await axios.get(`${baseApi}/class/all`).then(res => {
       setClasses(res.data.data.length)
       // console.log(classes)
-  }).catch(er=>{
-    console.log(er)
-  })
+    }).catch(er => {
+      console.log(er)
+    })
   }
   // fetchStudents
-  const fetchStudents=async()=>{
-  await  axios.get(`${baseApi}/student/fetch-with-query`).then(res=>{
-      console.log(res)
-  }).catch(er=>{
-    console.log(er)
-  })
+  const fetchStudents = async () => {
+    await axios.get(`${baseApi}/student/fetch-with-query`).then(res => {
+      // console.log(res)
+      setStudents(res.data.students.length)
+    }).catch(er => {
+      console.log(er)
+    })
   }
- 
+  const fetchStudentsDetails = async () => {
+    await axios.get(`${baseApi}/teacher/fetch-with-query`).then(res => {
+      // console.log(res)
+      setStudentsDetails(res.data.teachers)
+    }).catch(er => {
+      console.log(er)
+    })
+  }
+  console.log(studentsDetails)
+
+  // fetchStudents
+  const fetchExams = async () => {
+    await axios.get(`${baseApi}/examination/all`).then(res => {
+      // console.log(res)
+      setAllExamination(res.data.allExamination.length)
+    }).catch(er => {
+      console.log(er)
+    })
+  }
+  // console.log(examinations)
 
 
-  
+
+
+
   useEffect(() => {
     fetchSchool();
     fetchNumberOfClasses()
     fetchStudents()
+    fetchExams()
+    fetchStudentsDetails()
   }, []);
+
+  const pieData = [
+    { id: "Students", value: students },
+    { id: "Classes", value: classes },
+    { id: "Examinations", value: examinations }
+  ];
 
 
 
@@ -113,9 +149,9 @@ function Dashboard() {
             sx={{
               display: 'flex',
               flexDirection: 'column',
-              minWidth:'230px',
-              width:'50vw',
-              margin:'auto'
+              minWidth: '230px',
+              width: '50vw',
+              margin: 'auto'
             }}
             noValidate
             autoComplete="off"
@@ -144,14 +180,14 @@ function Dashboard() {
 
             <TextField
               variant="outlined"
-             sx={{marginTop:'8px',fontFamily: 'Courier New' }}
-             label='School Name'
+              sx={{ marginTop: '8px', fontFamily: 'Courier New' }}
+              label='School Name'
               fullWidth
               value={schoolName}
-              onChange={(e)=>setSchoolName(e.target.value)}
+              onChange={(e) => setSchoolName(e.target.value)}
             />
-           <Button onClick={handleEditSubmit} sx={{color:'white',background:'lightGreen'}}>Submit Edit</Button>
-           <Button variant='outlined' onClick={handleCancelEdit} sx={{color:'black',marginBottom:'10px'}}>Cancel</Button>
+            <Button onClick={handleEditSubmit} sx={{ color: 'white', background: 'lightGreen' }}>Submit Edit</Button>
+            <Button variant='outlined' onClick={handleCancelEdit} sx={{ color: 'black', marginBottom: '10px' }}>Cancel</Button>
           </Box>
         </>
       )}
@@ -164,13 +200,13 @@ function Dashboard() {
             background: `url(${school.school_image})`,
             backgroundSize: 'cover',
             display: 'flex',
-            backgroundPosition:'center',
+            backgroundPosition: 'center',
             alignItems: 'center',
             justifyContent: 'center',
             position: 'relative',
           }}
         >
-          <Typography variant="h2" style={{ color: 'black',fontFamily:'Courier New' }}>
+          <Typography variant="h2" style={{ color: 'black', fontFamily: 'Courier New' }}>
             {school.school_name}
           </Typography>
           <Box
@@ -196,7 +232,60 @@ function Dashboard() {
             </Button>
           </Box>
         </Box>
+
       )}
+
+      <Typography sx={{ fontSize: '24px', fontFamily: 'New courier', textAlign: 'center' }}>
+        Student Index
+      </Typography>
+      <Paper sx={{ height: '500px', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Box sx={{ height: '400px', width: '80%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <PieChart
+            series={[
+              {
+                data: [
+                  { id: "Students", value: students, label: `Students: ${students}` },
+                  { id: "Classes", value: classes, label: `Classes: ${classes}` },
+                  { id: "Examinations", value: examinations, label: `Exams: ${examinations}` }
+                ],
+                highlightScope: { fade: 'global', highlight: 'item' },
+                faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' }
+              }
+            ]}
+            height={250}
+            sx={{ padding: '5px' }}
+            tooltip={{ trigger: 'item' }}
+          />
+        </Box>
+      </Paper>
+      <Typography sx={{ fontSize: '24px', fontFamily: 'New courier', textAlign: 'center' }}>
+        Meet Our Faculty
+      </Typography>
+      {
+        <Box component={'div'} sx={{ display: 'flex', flexWrap: 'wrap' }}>
+          {
+            studentsDetails ? studentsDetails.map((students) => (
+              <Card key={students._id} sx={{ maxWidth: 280, margin: 4 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="260"
+                  image={`${students.teacher_image}`}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    <span style={{ fontWeight: '800' }}>Name:</span> {students.name}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    <span style={{ fontWeight: '800' }}>Qualification:</span> { students.qualification}
+                  </Typography>
+                </CardContent>
+              </Card>
+            )) : ''
+          }
+        </Box>
+      }
+
     </>
   );
 }
